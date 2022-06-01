a289b4a39f5f8a59f5b36daf781d0e51899bc32e
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-01 20:39:16 +0200

    feat(mpa): order project list alphabetically

--- src/mpa/views/project_list.py
+++ src/mpa/views/project_list.py
@@ -3,6 +3,6 @@ from app.models import Project
 
 
 def project_list(request):
-    projects = Project.objects.filter(is_deleted=False)
+    projects = Project.objects.filter(is_deleted=False).order_by("name")
     context = {"project_list": projects}
     return render(request, "mpa/project/project_list.html", context)
