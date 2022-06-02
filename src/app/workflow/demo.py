bf30a301627f109f9902e890248bee229810c561
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-02 19:45:36 +0200

    feat(mpa): show example events

--- src/app/workflow/demo.py
+++ src/app/workflow/demo.py
@@ -5,6 +5,7 @@ import logging
 import uuid
 
 import app.models.example
+from app.models.example_event import ExampleEvent
 import app.integrations.label_studio as ls
 
 logger = logging.getLogger(__package__)
@@ -21,7 +22,7 @@ def start(project_id: uuid.UUID, example_id: uuid.UUID) -> None:
 def create_labeling_task(project_id: uuid.UUID, example_id: uuid.UUID) -> None:
     # TODO: replace me by project-level Label Studio plugin configuration
     LABELSTUDIO_URL = "http://host.docker.internal:8080/"
-    LABELSTUDIO_API_KEY = "d3bca97b95da0820cadae2197c7ccde4ee6e77b7"
+    LABELSTUDIO_API_KEY = "d3bca97b95da0820cadae2197c7ccde4ee6e77b"
     LABELSTUDIO_PROJECT_ID = "2"
 
     media_urls = app.models.example.Example.objects.filter(id=example_id).values_list(
@@ -36,9 +37,19 @@ def create_labeling_task(project_id: uuid.UUID, example_id: uuid.UUID) -> None:
     success, message = ls.create_image_labeling_task(
         LABELSTUDIO_URL, LABELSTUDIO_API_KEY, LABELSTUDIO_PROJECT_ID, media_urls[0]
     )
-    status = (
-        app.models.Example.Status.started
-        if success
-        else app.models.Example.Status.error
-    )
-    app.models.Example.objects.filter(id=example_id).update(status=status)
+    if success:
+        app.models.Example.objects.filter(id=example_id).update(
+            status=app.models.Example.Status.started
+        )
+        ExampleEvent.objects.create(
+            example_id=example_id, event_type=ExampleEvent.EventType.started
+        )
+    else:
+        app.models.Example.objects.filter(id=example_id).update(
+            status=app.models.Example.Status.error
+        )
+        ExampleEvent.objects.create(
+            example_id=example_id,
+            event_type=ExampleEvent.EventType.error,
+            properties={"message": message},
+        )
--- src/mpa/views/example_detail.py
+++ src/mpa/views/example_detail.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from app.models import Project, Example
+from app.models import Project, Example, ExampleEvent
 
 
 @login_required
@@ -9,5 +9,6 @@ def example_detail(request, project_id, example_id):
     example = get_object_or_404(
         Example, id=example_id, project=project, is_deleted=False
     )
-    context = {"project": project, "example": example}
+    event_list = ExampleEvent.objects.filter(example_id=example_id).order_by("created_at")
+    context = {"project": project, "example": example, "event_list": event_list}
     return render(request, "mpa/example/example_detail.html", context)
