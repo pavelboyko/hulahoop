3706d670d18d6c6e74a3efb4d3151c42d652d3d1
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-05 14:37:45 +0200

    feat(mpa): remove example list filter for a while, order by -created_at

--- src/mpa/views/example_list.py
+++ src/mpa/views/example_list.py
@@ -1,23 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-import django_filters
 from app.models import Project, Example
-
-
-class ExampleFilter(django_filters.FilterSet):
-    created_at = django_filters.DateRangeFilter()
-
-    class Meta:
-        model = Example
-        fields = ["status", "created_at"]
 
 
 @login_required
 def example_list(request, project_id):
     project = get_object_or_404(Project, id=project_id, is_deleted=False)
-    example_filter = ExampleFilter(
-        request.GET, queryset=Example.objects.filter(project=project, is_deleted=False)
-    )
-
-    context = {"project": project, "example_filter": example_filter}
-    return render(request, "mpa/example/example_list.html", context)
+    examples = Example.objects.filter(is_deleted=False, project=project).order_by("-created_at")
+    return render(request, "mpa/example/example_list.html", {"project": project, "examples": examples})
