5c121dc6ded75f0ced905b6cad6deb3eec29ceb9
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-05 15:12:23 +0200

    feat(mpa): print project 30 days example statistics on the dashboard page

--- src/mpa/views/example_list.py
+++ src/mpa/views/example_list.py
@@ -6,5 +6,11 @@ from app.models import Project, Example
 @login_required
 def example_list(request, project_id):
     project = get_object_or_404(Project, id=project_id, is_deleted=False)
-    examples = Example.objects.filter(is_deleted=False, project=project).order_by("-created_at")
-    return render(request, "mpa/example/example_list.html", {"project": project, "examples": examples})
+    examples = Example.objects.filter(is_deleted=False, project=project).order_by(
+        "-created_at"
+    )
+    return render(
+        request,
+        "mpa/example/example_list.html",
+        {"project": project, "examples": examples},
+    )
--- src/mpa/views/project_dashboard.py
+++ src/mpa/views/project_dashboard.py
@@ -1,9 +1,28 @@
+from django.utils import timezone
+from datetime import timedelta
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from app.models import Project
+from django.db.models import Count
+from app.models import Project, Example
 
 
 @login_required
 def project_dashboard(request, project_id):
     project = get_object_or_404(Project, id=project_id, is_deleted=False)
-    return render(request, "mpa/project/project_dashboard.html", {"project": project})
+    examples_last_30_days = (
+        Example.objects.filter(
+            project=project,
+            is_deleted=False,
+            created_at__gte=timezone.now() - timedelta(days=30),
+        )
+        .values("created_at__date", "status")
+        .annotate(count=Count("id"))
+        .values("created_at__date", "status", "count")
+        .order_by("created_at__date")
+    )
+
+    return render(
+        request,
+        "mpa/project/project_dashboard.html",
+        {"project": project, "examples_last_30_days": examples_last_30_days},
+    )
