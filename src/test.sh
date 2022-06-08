c77abda09b170839fc0a838bd4776a98f31d5fb4
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-08 22:17:20 +0200

    test(app): test.sh improvements

--- src/test.sh
+++ src/test.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-coverage run --source='.' ./manage.py test --keepdb && coverage report && coverage html
+coverage run --source='.' ./manage.py test --keepdb && \
+coverage report --skip-covered --omit=hulahoop/* && \
+coverage html --skip-covered --omit=hulahoop/*
