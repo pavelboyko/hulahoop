bcce19facf448c0f6cf724299f096a524edd5b49
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-10 21:25:17 +0200

    feat(mpa): integration settings page

--- src/mpa/views/project/settings.py
+++ src/mpa/views/project/settings.py
@@ -1,28 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django import forms
 from app.models import Project
-
-
-class ProjectForm(forms.Form):
-    pass
+from hulahoop.settings import HTTP_SCHEME, HOSTNAME
 
 
 @login_required
 def project_settings(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
-
-    if request.method == "POST":
-        form = ProjectForm(request.POST)
-        if form.is_valid():
-            # TODO update project
-            return HttpResponseRedirect(
-                reverse("project_settings", kwargs={"project": project})
-            )
-    else:
-        form = ProjectForm()
-
+    capture_endpoint = (
+        f"{HTTP_SCHEME}{HOSTNAME}{reverse('api_capture', args=[project_id])}"
+    )
     return render(
-        request, "mpa/project/settings.html", {"project": project, "form": form}
+        request,
+        "mpa/project/settings.html",
+        {"project": project, "capture_endpoint": capture_endpoint},
     )
