a8a923729d7a5d406ee5cb02da27a3f8364946ab
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-13 21:18:06 +0200

    feat(mpa): display example predictions, annotations and properties

--- src/mpa/views/example/detail.py
+++ src/mpa/views/example/detail.py
@@ -1,3 +1,5 @@
+from typing import Any
+import json
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from app.models import Project, Example
@@ -5,7 +7,19 @@ from app.models import Project, Example
 
 @login_required
 def example_detail(request, project_id, example_id):
+    def pretty_print(data: Any) -> str:
+        return json.dumps(data, indent=4, sort_keys=True) if data is not None else ""
+
     project = get_object_or_404(Project, id=project_id)
     example = get_object_or_404(Example, id=example_id, project=project)
-    context = {"project": project, "example": example}
-    return render(request, "mpa/example/detail.html", context)
+    return render(
+        request,
+        "mpa/example/detail.html",
+        {
+            "project": project,
+            "example": example,
+            "predictions": pretty_print(example.predictions),
+            "annotations": pretty_print(example.annotations),
+            "properties": pretty_print(example.properties),
+        },
+    )
