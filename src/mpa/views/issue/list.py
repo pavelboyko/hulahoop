1f06601786205bdcb1e11863825f7e727ed11628
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-15 01:23:50 +0200

    feat(mpa): cosmetics

--- src/mpa/views/issue/list.py
+++ src/mpa/views/issue/list.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 import django_filters
@@ -10,8 +11,9 @@ class IssueFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(
         lookup_expr="icontains", label="Search issues by name"
     )
-    examples__gt = django_filters.NumberFilter(
-        field_name="examples", lookup_expr="gt", label="Min examples"
+    status = django_filters.ChoiceFilter(choices=Issue.Status.choices)
+    examples__gte = django_filters.NumberFilter(
+        field_name="examples", lookup_expr="gte", label="Min examples"
     )
     order = django_filters.OrderingFilter(
         choices=(
@@ -24,7 +26,7 @@ class IssueFilter(django_filters.FilterSet):
 
     class Meta:
         model = Issue
-        fields = ["name"]
+        fields = ["name", "status"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -36,7 +38,9 @@ class IssueFilter(django_filters.FilterSet):
 @login_required
 def issue_list(request, project_id):
     project = get_object_or_404(Project, id=project_id)
-    issues = project.issue_set.annotate(examples=Count("example"))
+    issues = project.issue_set.filter(status=Issue.Status.open).annotate(
+        examples=Count("example")
+    )
     filter = IssueFilter(request.GET, queryset=issues)
     return render(
         request,
