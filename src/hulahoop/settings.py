d56ee72762ff4aa791cbb3580e8b60eb99d0c5b1
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-23 17:41:44 +0200

    feat(ci): use sqlite for unit tests

--- src/hulahoop/settings.py
+++ src/hulahoop/settings.py
@@ -89,17 +89,37 @@ WSGI_APPLICATION = "hulahoop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "db",
-        "PORT": 5432,
-    }
-}
+POSTGRES_NAME = os.environ.get("POSTGRES_NAME")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 
+HAS_POSTGRES = all(
+    [
+        POSTGRES_NAME,
+        POSTGRES_USER,
+        POSTGRES_PASSWORD,
+    ]
+)
+
+if HAS_POSTGRES:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": POSTGRES_NAME,
+            "USER": POSTGRES_USER,
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": "db",
+            "PORT": 5432,
+        }
+    }
+else:
+    # this configurations is used for testing
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": "db",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
