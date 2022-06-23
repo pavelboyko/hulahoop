a434ce83033647133193dd8f093fa43ed890f5b7
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-23 20:12:11 +0200

    tests(app): added celery test fixtures to fix unit tests in github actions worker

--- src/app/api/tests/test_webhook.py
+++ src/app/api/tests/test_webhook.py
@@ -1,13 +1,28 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
 from rest_framework.test import APIClient
 from app.models import Project
 from app.fixtures import ProjectFactory
 
+# need this because webhooks under test are processed in a celery task
+from hulahoop.celery import app
+from celery.contrib.testing.worker import start_worker
 
-class Test(TestCase):
+
+class Test(TransactionTestCase):
     """
     ./manage.py test app.api.tests.test_webhook.Test --keepdb --verbosity 2
     """
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.celery_worker = start_worker(app, perform_ping_check=False)
+        cls.celery_worker.__enter__()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.celery_worker.__exit__(None, None, None)
 
     def tearDown(self) -> None:
         Project.objects.all().delete()
