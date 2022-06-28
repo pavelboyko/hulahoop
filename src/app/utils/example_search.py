713bbecc361d58758670f850b1e017e2f0fabc40
Pavel Boyko <pavel.boyko@gmail.com>
2022-06-28 22:36:31 +0200

    refactor(app): change data structures inside example search query

--- src/app/utils/example_search.py
+++ src/app/utils/example_search.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import Dict
 from dataclasses import dataclass
 import shlex
 from django.db.models import Q
@@ -9,16 +9,10 @@ from app.models import Example, Tag
 logger = logging.getLogger(__package__)
 
 
-@dataclass(frozen=True)
-class KV:
-    key: str
-    value: str
-
-
-@dataclass(frozen=True)
+@dataclass
 class ExampleSearchQuery:
-    tags: List[KV]
-    fields: List[KV]
+    tags: Dict[str, str]
+    fields: Dict[str, str]
     random: int | None = None
 
 
@@ -43,8 +37,8 @@ def parse_query_string(query: str) -> ExampleSearchQuery:
     except ValueError as e:
         raise ParsingError(f"Failed to parse query string: {e}") from e
 
-    tags = []
-    fields = []
+    tags = {}
+    fields = {}
     random = None
 
     for term in terms:
@@ -53,7 +47,7 @@ def parse_query_string(query: str) -> ExampleSearchQuery:
             raise ParsingError(f"Invalid search query: {term}. Expected key=value.")
 
         if tokens[0][:5] == "tag__":
-            tags.append(KV(tokens[0][5:], tokens[1]))
+            tags[tokens[0][5:]] = tokens[1]
         elif tokens[0] == "random":
             try:
                 random = int(tokens[1])
@@ -71,18 +65,16 @@ def parse_query_string(query: str) -> ExampleSearchQuery:
                 raise ParsingError(
                     f"Invalid search query: {term}. Uknown field {tokens[0]}."
                 )
-            fields.append(KV(tokens[0], tokens[1]))
+            fields[tokens[0]] = tokens[1]
 
     return ExampleSearchQuery(tags, fields, random)
 
 
 def query_to_Q(query: ExampleSearchQuery) -> Q:
     q = Q()
-    for field in query.fields:
-        q = q & Q(**{field.key: field.value})
-
-    for tag in query.tags:
-        q = q & Q(tag__key=tag.key, tag__value=tag.value)
+    q = q & Q(**query.fields)
+    for key, value in query.tags.items():
+        q = q & Q(tag__key=key, tag__value=value)
 
     return q
 
@@ -92,7 +84,7 @@ def query_to_string(query: ExampleSearchQuery) -> str:
         return f'"{s}"' if " " in s else s
 
     return " ".join(
-        [f"{q(field.key)}={q(field.value)}" for field in query.fields]
-        + [f"tag__{q(tag.key)}={q(tag.value)}" for tag in query.tags]
+        [f"{q(key)}={q(value)}" for key, value in query.fields.items()]
+        + [f"tag__{q(key)}={q(value)}" for key, value in query.tags.items()]
         + [f"random={query.random}" if query.random is not None else ""]
     ).strip()
--- src/app/utils/tests/test_example_search.py
+++ src/app/utils/tests/test_example_search.py
@@ -3,7 +3,6 @@ from app.models import Example, Tag
 from app.fixtures import ExampleFactory
 from app.utils.example_search import (
     ExampleSearchQuery,
-    KV,
     parse_query_string,
     query_to_Q,
     query_to_string,
@@ -40,27 +39,19 @@ class Test(TestCase):
 
     def test_parse_tag(self) -> None:
         query = parse_query_string("tag__a=b")
-        self.assertEqual(len(query.tags), 1)
-        self.assertEqual(query.tags[0].key, "a")
-        self.assertEqual(query.tags[0].value, "b")
+        self.assertDictEqual(query.tags, {"a": "b"})
 
     def test_parse_field(self) -> None:
         query = parse_query_string("fingerprint=qqq")
-        self.assertEqual(len(query.fields), 1)
-        self.assertEqual(query.fields[0].key, "fingerprint")
-        self.assertEqual(query.fields[0].value, "qqq")
+        self.assertDictEqual(query.fields, {"fingerprint": "qqq"})
 
     def test_parse_field_subfield(self) -> None:
         query = parse_query_string("annotations__label=qqq")
-        self.assertEqual(len(query.fields), 1)
-        self.assertEqual(query.fields[0].key, "annotations__label")
-        self.assertEqual(query.fields[0].value, "qqq")
+        self.assertDictEqual(query.fields, {"annotations__label": "qqq"})
 
     def test_parse_field_subsubfield(self) -> None:
         query = parse_query_string("annotations__choices__0=qqq")
-        self.assertEqual(len(query.fields), 1)
-        self.assertEqual(query.fields[0].key, "annotations__choices__0")
-        self.assertEqual(query.fields[0].value, "qqq")
+        self.assertDictEqual(query.fields, {"annotations__choices__0": "qqq"})
 
     def test_parse_random(self) -> None:
         query = parse_query_string("random=100")
@@ -68,35 +59,35 @@ class Test(TestCase):
 
     def test_parse_whitespace_value(self) -> None:
         query = parse_query_string('tag__a="x y"')
-        self.assertEqual(len(query.tags), 1)
-        self.assertEqual(query.tags[0].key, "a")
-        self.assertEqual(query.tags[0].value, "x y")
+        self.assertDictEqual(query.tags, {"a": "x y"})
 
     def test_parse_whitespace_key(self) -> None:
         query = parse_query_string('"tag__a b"=c')
-        self.assertEqual(len(query.tags), 1)
-        self.assertEqual(query.tags[0].key, "a b")
-        self.assertEqual(query.tags[0].value, "c")
+        self.assertDictEqual(query.tags, {"a b": "c"})
+
+    def test_parse_double(self) -> None:
+        query = parse_query_string("tag__a=b tag__a=c")
+        self.assertDictEqual(query.tags, {"a": "c"})
 
     def test_str_empty(self) -> None:
-        query = ExampleSearchQuery([], [])
+        query = ExampleSearchQuery({}, {})
         self.assertEqual(query_to_string(query), "")
 
     def test_str(self) -> None:
         query = ExampleSearchQuery(
-            tags=[KV("a", "b"), KV("c", "d")],
-            fields=[KV("annotations", "qqq"), KV("predictions", "ppp")],
+            tags={"a": "b", "c": "d"},
+            fields={"fingerprint": "qqq", "predictions": "ppp"},
             random=100,
         )
         self.assertEqual(
             query_to_string(query),
-            "annotations=qqq predictions=ppp tag__a=b tag__c=d random=100",
+            "fingerprint=qqq predictions=ppp tag__a=b tag__c=d random=100",
         )
 
     def test_str_whitespace(self) -> None:
         query = ExampleSearchQuery(
-            tags=[KV("a", "x y"), KV("b", "x y z")],
-            fields=[KV("annotations", "xxx yyy"), KV("predictions", "xxx yyy zzz")],
+            tags={"a": "x y", "b": "x y z"},
+            fields={"annotations": "xxx yyy", "predictions": "xxx yyy zzz"},
         )
         self.assertEqual(
             query_to_string(query),
@@ -107,7 +98,7 @@ class Test(TestCase):
         ex1: Example = ExampleFactory.create(tags=0)
         Tag.objects.create(example=ex1, key="a", value="b")
         ExampleFactory(tags=0)
-        query = ExampleSearchQuery(tags=[KV("a", "b")], fields=[])
+        query = ExampleSearchQuery(tags={"a": "b"}, fields={})
         qs = Example.objects.filter(query_to_Q(query))
         self.assertEqual(qs.count(), 1)
         self.assertEqual(qs[0].id, ex1.id)
@@ -115,7 +106,7 @@ class Test(TestCase):
     def test_Q_fields(self) -> None:
         ex1: Example = ExampleFactory.create(tags=0, fingerprint="qqq")
         ExampleFactory(tags=0, fingerprint="ppp")
-        query = ExampleSearchQuery(tags=[], fields=[KV("fingerprint", "qqq")])
+        query = ExampleSearchQuery(tags={}, fields={"fingerprint": "qqq"})
         qs = Example.objects.filter(query_to_Q(query))
         self.assertEqual(qs.count(), 1)
         self.assertEqual(qs[0].id, ex1.id)
