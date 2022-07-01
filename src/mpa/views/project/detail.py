6915639e4d9dd294d8c60ab73ef149ab98440e9c
Pavel Boyko <pavel.boyko@gmail.com>
2022-07-01 21:00:52 +0200

    feat(mpa): link to the list of open issues by default

--- src/mpa/views/project/detail.py
+++ src/mpa/views/project/detail.py
@@ -6,5 +6,5 @@ from django.urls import reverse
 @login_required
 def project_detail(request, project_id):
     return HttpResponseRedirect(
-        reverse("issue_list", kwargs={"project_id": project_id})
+        reverse("issue_list", kwargs={"project_id": project_id}) + "?status=0"
     )
